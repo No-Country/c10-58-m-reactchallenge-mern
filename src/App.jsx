af9a9fc508c3ae6ea4d7e2a318df59873599fbc5
snowingt <snowingtorres@gmail.com>
2023-03-28 11:25:20 -0400

    signCorreccion

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
+import React from 'react'
+import SingIn from './components/SigngIn'
 
 function App () {
   return (
     <div className='App'>
       <h1>Hola mundo</h1>
-     
+     <SingIn/>
     </div>
   )
 }
