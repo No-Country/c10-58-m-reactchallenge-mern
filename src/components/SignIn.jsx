76e26425916f16e43c2b31903711f27dfcda98a1
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2023-03-28 12:45:13 -0300

    fix component name

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import styled from "styled-components";
-//styles
+import React from 'react'
+import styled from 'styled-components'
+// styles
 const H1 = styled.h1`
   color: #000;
   font-size: 40px;
   font-weight: 700;
   margin: 10px;
-`;
+`
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -17,7 +17,7 @@ const Container = styled.div`
   height: 500px;
   width: 350px;
   border-radius: 10px;
-`;
+`
 const Form = styled.form`
   display: flex;
   flex-direction: column;
@@ -54,7 +54,7 @@ const Form = styled.form`
     background-color: #000;
     color: #fff;
   }
-`;
+`
 const ButtonRed = styled.button`
   background-color: #fff;
   color: #000000;
@@ -67,18 +67,18 @@ const ButtonRed = styled.button`
     background-color: #000;
     color: #fff;
   }
-`;
+`
 const P = styled.p`
   margin: 10px;
   color: #000;
   font-size: 20px;
-`;
+`
 const Span = styled.span`
   margin: 10px;
   color: #000;
   font-size: 20px;
  
-`;
+`
 const DivLine = styled.div`
   display: flex;
   flex-direction: row;
@@ -97,68 +97,62 @@ const DivLine = styled.div`
     height: 1px;
     background-color: #000;
   }
-`;
-
+`
 
 const SignIn = () => {
-  //add function for google and facebook
-  const handleSubmitForGoogle=(e)=>{
-    if(e.target.value==='1'){
+  // add function for google and facebook
+  const handleSubmitForGoogle = (e) => {
+    if (e.target.value === '1') {
       console.log('google')
-    }else{
+    } else {
       console.log('facebook')
     }
-    
   }
-  //form validation
+  // form validation
   const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("submit");
-   
-    //validate
-    if(e.target.email.value === "" || e.target.password.value === ""){
+    e.preventDefault()
+    console.log('submit')
+
+    // validate
+    if (e.target.email.value === '' || e.target.password.value === '') {
       console.log('empty')
-      return;
+      return
     }
-    //send data
+    // send data
     console.log(e.target.email.value)
 
-    //clean form
-    e.target.email.value = "";
-    e.target.password.value = "";
-
-
-  };
-  //input change
+    // clean form
+    e.target.email.value = ''
+    e.target.password.value = ''
+  }
+  // input change
   const handleChange = (e) => {
-    e.target.value;
-  };
+    e.target.value
+  }
 
   return (
-    <Container className="sing--in flex-col">
-      <H1 className="text-4xl">Sign In</H1>
-    
+    <Container className='sing--in flex-col'>
+      <H1 className='text-4xl'>Sign In</H1>
 
-      <Form onSubmit={handleSubmit} className="flex">
-        <label htmlFor="email">Email</label>
-        <input onChange={handleChange} type="email" name="email" id="email" />
-        <label htmlFor="password">Password</label>
-        <input onChange={handleChange} type="password" name="password" id="password" />
-        <button type="submit">Sing In</button>
+      <Form onSubmit={handleSubmit} className='flex'>
+        <label htmlFor='email'>Email</label>
+        <input onChange={handleChange} type='email' name='email' id='email' />
+        <label htmlFor='password'>Password</label>
+        <input onChange={handleChange} type='password' name='password' id='password' />
+        <button type='submit'>Sing In</button>
       </Form>
-     
-      <DivLine><hr/><span>or</span><hr/></DivLine>
+
+      <DivLine><hr /><span>or</span><hr /></DivLine>
       <div>
-        <ButtonRed onClick={handleSubmitForGoogle} value="1" className=""> G</ButtonRed>
-        <ButtonRed onClick={handleSubmitForGoogle} value="2" className="">F</ButtonRed>
+        <ButtonRed onClick={handleSubmitForGoogle} value='1' className=''> G</ButtonRed>
+        <ButtonRed onClick={handleSubmitForGoogle} value='2' className=''>F</ButtonRed>
       </div>
 
-
-      <div className="">
-        <P className="text-2xl">Don't have an account?</P>
+      <div className=''>
+        <P className='text-2xl'>Don't have an account?</P>
       </div>
     </Container>
-  );
-};
+  )
+}
 
-export default SignIn;
+export default SignIn
