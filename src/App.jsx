01febe84006b35caf7665d30277d12eec8be40d4
uspaz <Matias-Teijeiro2015@hotmail.com>
2023-03-29 12:34:22 -0300

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable */
-import { Outlet } from 'react-router-dom';
-import { TestingValentin } from './firebase/testValentin';
+import { Outlet } from "react-router-dom";
+import { TestingValentin } from "./firebase/testValentin";
 
 function App() {
-	return (
-		<div className='App'>
-			<h1>APP</h1>
-			<Outlet />
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1>APP</h1>
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
