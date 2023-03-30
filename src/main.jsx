15f9abdef2ccbed301925c577eb800b50ba68420
snowingt <snowingtorres@gmail.com>
2023-03-30 13:04:52 -0400

    bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-<<<<<<< HEAD
-import { RouterProvider } from 'react-router-dom'
-import { router } from './router/router'
-import FirebaseProvider from './context/UserContext'
-=======
 import FirebaseProvider from './context/UserContext'
 import App from './App'
->>>>>>> dev
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <FirebaseProvider>
-<<<<<<< HEAD
-      <RouterProvider router={router} />
-=======
       <App />
->>>>>>> dev
     </FirebaseProvider>
   </React.StrictMode>
 )
