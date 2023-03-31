9c610bdb4ea824e146d3bee04fa7df67f48b9455
snowingt <snowingtorres@gmail.com>
2023-03-30 22:23:14 -0400

    temporal sing

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+// styles
+const ButtonRed = styled.button`
+  background-color: #fff;
+  color: #000000;
+  width: 70px;
+  height: 60px;
+  border: 1px solid #000;
+  border-radius: 5px;
+  margin: 10px;
+  &:hover {
+    background-color: #000;
+    color: #fff;
+  }
+`
+
 
 const SignIn = () => {
   return (
     <div>
       <h1>SignIn</h1>
-      <Link to="/esteban">Register</Link>
+      <Link to="/esteban">
+      <ButtonRed>
+
+     click to register
+      </ButtonRed>
+      </Link>
     </div>
   )
 }
