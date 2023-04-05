57d340d7a5b126354e0097d132df3745a26e6905
estebanzarate <estebanzaratedev@gmail.com>
2023-04-05 13:27:22 -0300

    Fix router

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,23 +1,45 @@
 /* eslint-disable */
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from '../components/Home'
-import Register from '../components/Register'
-import SignIn from '../components/SignIn'
-import CardContainer from '../components/CardContainer'
-import HomeTemp from '../components/HomeTemp'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from '../pages/Home';
+import Register from '../components/Register';
+import SignIn from '../components/SignIn';
+import App from '../App';
+import List from '../pages/List';
+import Profile from '../pages/Profile';
 
-
-const Router = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/esteban" element={<Register />} />
-        <Route path="/matias" element={<Home />} />
-        <Route path="/card" element={<CardContainer />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
-
-export default Router
+export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{
+				index: true,
+				element: <Home />
+			},
+			{
+				path: '/list',
+				element: <List />
+			},
+			{
+				path: '/list/:id',
+				element: <h1>Perfil Médico</h1>
+			},
+			{
+				path: '/list/:id/calendar',
+				element: <h1>Calendario</h1>
+			},
+			{
+				path: '/login',
+				element: <SignIn />
+			},
+			{
+				path: '/register',
+				element: <Register />
+			},
+			{
+				path: '/profile',
+				element: <Profile />
+			}
+		]
+	}
+]);
