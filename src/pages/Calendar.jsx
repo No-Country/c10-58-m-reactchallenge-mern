14fe86f03316c50b62738479ae6b83ef631aaf79
estebanzarate <estebanzaratedev@gmail.com>
2023-04-06 00:44:47 -0300

    Add content and styles to component Calendar

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable */
 
 import { Link } from 'react-router-dom';
-import { fetchCollection } from '../firebase/fetchCollection';
 import { Btn } from '../components/MicroComponents/Btn';
 
 const Calendar = () => {
@@ -16,8 +15,8 @@ const Calendar = () => {
 			<p className='text-xs text-center my-4'>
 				Elige el dia y la hora en la que quieres tu cita
 			</p>
-			<div className='p-4'>
-				<h3 className='font-semibold'>Abril</h3>
+			<div className='max-w-sm mx-auto p-4 mb-12'>
+				<h3 className='px-4 text-xl font-semibold'>Abril</h3>
 				<p className='text-2xl text-right'>{'>'}</p>
 				<div className='grid grid-cols-5 justify-items-center'>
 					{[
@@ -27,9 +26,31 @@ const Calendar = () => {
 						{ id: 4, day: 'Jue' },
 						{ id: 5, day: 'Vie' }
 					].map(({ id, day }) => (
-						<ul key={id} className='font-medium'>
-							{day}
-						</ul>
+						<div key={id}>
+							<h4 className='font-medium text-center text-xs mb-4'>{day}</h4>
+							<ul>
+								{[
+									{ id: 1, hour: '7:00' },
+									{ id: 2, hour: '8:00' },
+									{ id: 3, hour: '9:00' },
+									{ id: 4, hour: '10:00' },
+									{ id: 5, hour: '11:00' },
+									{ id: 6, hour: '12:00' },
+									{ id: 7, hour: '13:00' },
+									{ id: 8, hour: '14:00' },
+									{ id: 9, hour: '15:00' },
+									{ id: 10, hour: '16:00' },
+									{ id: 11, hour: '17:00' },
+									{ id: 12, hour: '18:00' }
+								].map(({ id, hour }) => (
+									<li
+										className='border-[1px] border-gray-400 rounded-md py-0 px-2 my-2 text-xs text-center cursor-pointer'
+										key={id}>
+										{hour}
+									</li>
+								))}
+							</ul>
+						</div>
 					))}
 				</div>
 			</div>
