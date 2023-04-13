f2a6afc323aaa6af09b7c6e98f402bb9be5acf18
estebanzarate <estebanzaratedev@gmail.com>
2023-04-13 01:06:23 -0300

    Add loader and component ProfileDoc

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,56 +1,62 @@
 /* eslint-disable */
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from '../pages/Home'
-import Register from '../pages/Register'
-import SignIn from '../pages/SignIn'
-import App from '../App'
-import List from '../pages/List'
-import Profile from '../pages/Profile'
-import Calendar from '../pages/Calendar'
-import Emergency from '../pages/Emergency'
-import { TestingValentin } from '../firebase/testValentin'
+import { createBrowserRouter } from 'react-router-dom';
+import Home from '../pages/Home';
+import Register from '../pages/Register';
+import SignIn from '../pages/SignIn';
+import App from '../App';
+import List from '../pages/List';
+import Profile from '../pages/Profile';
+import Calendar from '../pages/Calendar';
+import Emergency from '../pages/Emergency';
+import { TestingValentin } from '../firebase/testValentin';
+import ProfileDoc from '../pages/ProfileDoc';
+import { getMedicData } from '../firebase/medics';
 
 export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: '/list',
-        element: <List />,
-      },
-      {
-        path: '/list/:id',
-        element: <h1>Perfil Médico</h1>,
-      },
-      {
-        path: '/list/:id/calendar',
-        element: <Calendar />,
-      },
-      {
-        path: '/login',
-        element: <SignIn />,
-      },
-      {
-        path: '/register',
-        element: <Register />,
-      },
-      {
-        path: '/profile',
-        element: <Profile />,
-      },
-      {
-        path: '/emergency',
-        element: <Emergency />,
-      },
-      {
-        path: '/valentin',
-        element: <TestingValentin />,
-      }
-    ],
-  },
-])
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{
+				index: true,
+				element: <Home />
+			},
+			{
+				path: '/list',
+				element: <List />
+			},
+			{
+				path: '/list/:medicId',
+				element: <ProfileDoc />,
+				loader: async ({ params: { medicId } }) => {
+					const data = await getMedicData(medicId);
+					return { medicId, data };
+				}
+			},
+			{
+				path: '/list/:id/calendar',
+				element: <Calendar />
+			},
+			{
+				path: '/login',
+				element: <SignIn />
+			},
+			{
+				path: '/register',
+				element: <Register />
+			},
+			{
+				path: '/profile',
+				element: <Profile />
+			},
+			{
+				path: '/emergency',
+				element: <Emergency />
+			},
+			{
+				path: '/valentin',
+				element: <TestingValentin />
+			}
+		]
+	}
+]);
