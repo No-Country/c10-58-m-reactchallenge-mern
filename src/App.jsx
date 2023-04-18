32f820847734c854e21fd3def1c75c1ba3994eb4
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2023-04-18 20:14:24 -0300

    Add svgr dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 /* eslint-disable */
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Header from './components/Header'
+import {ReactComponent as BackArrow} from './assets/svgs/arrowleft.svg'
 
 function App() {
+  const navigate = useNavigate()
+
+  function navigateBack () {
+    navigate(-1)
+  }
+
   return (
-    <main className='w-full min-h-screen'>
+    <main className='w-screen min-h-screen flex flex-col justify-center pb-[50px]'>
+      <button className='absolute top-4 left-4' onClick={navigateBack}><BackArrow height={32} width={32}/></button>
       <Outlet />
       <Header />
-      <div className='h-[60px]'/>
     </main>
   )
 }
