1e3771a0732f15f9e3cf07c6a37a82355063f342
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2023-04-18 20:15:52 -0300

    edit profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,119 +1,33 @@
-import React, { useEffect } from 'react'
-import { Form } from '../components/MicroComponents/Form.js'
-import { Btn } from '../components/MicroComponents/Btn.js'
+import React from 'react'
+import { Navigate } from 'react-router-dom'
 import { useFirebaseContext } from '../context/UserContext'
-import { Navigate, useNavigate } from 'react-router-dom'
+import { Btn } from '../components/MicroComponents/Btn.js'
+import { ProfileImg } from '../components/MicroComponents/ProfileImages'
 
-import styled from 'styled-components'
-// styles
-
-const ButonLogout = styled.button`
-  color: #000;
-  font-size: 20px;
-  font-weight: 700;
-  margin: 10px;
-  padding-left: 10px;
-  border: 1px solid #000;
-`
-// imagen de perfil redonda
-const DivImg = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100px;
-  height: 100px;
-  margin: 10px;
-  border-radius: 15px;
-  background-color: #f6f6f6;
-  
-  img {
-    color:#f6f6f6;
-    width: 100%;
-    height: 100%;
-    border-radius: 15px;
-  }
-
-`
-const SpanAddImg = styled.span`
-  color: #ffffff;
-  font-size: 30px;
-  font-weight: 700;
-  margin: 10px;
-  padding-left: 10px;
-  border-radius: 100%;
-  background-color: #ecdcdc;
-  width: 20px;
-  height: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
-
-const Profile = () => {
+export const Profile = () => {
   const { user, logout } = useFirebaseContext()
-  console.log(user)
-  const navigate = useNavigate()
-  //destructuring
-  const {names,email,lastName,dni} = user
- 
-
-  const handleLogout = () => {
-    logout()
-    navigate('/login')
-  }
-  // validate form
-  const handleSubmit = (e) => {
-    e.preventDefault()
-   
-  }
-  // change input
-  const handleChange = (e) => {
-    e.preventDefault()
-
-
-  }
-
-  useEffect(() => {
-    !user && navigate('/', { replace: true })
-  }, [])
+  const { firstName, lastName } = user
 
   return (
-    <div>
-      {
-     user
-       ? (
-         <div className='w-full  flex justify-center text-black mb-10 '>
-           <div className='w-5/6  '>
-             <div className='flex  w-full h-52 place-content-center place-items-center '>
-               <DivImg>
-                 <img src={user.avatarURL} alt='perfil-img' />
-               </DivImg>
-               <div>
-
-               <h2>{names} {lastName}</h2>
-             <Btn className='mt-5' onClick={handleLogout}>Log Out</Btn>
-               </div>
-             </div>
-             <hr className='mb-5' />
-             <div className='justify-center w-full '>
-               <Form onSubmit={handleSubmit} className='flex flex-col w-full '>
-                 <input onChange={handleChange} type='text' placeholder={names} />
-                 <input onChange={handleChange} type='text' placeholder={lastName} />
-                 <input onChange={handleChange} type='text' placeholder={email} />
-                 <input onChange={handleChange} type='text' placeholder= {`DNI: ${dni}`} />
-                 <input onChange={handleChange} type='text' placeholder='Pais' />
-                 <input onChange={handleChange} type='text' placeholder='Ciudad' />
-                 <Btn type='submit' className='mt-20'>Guardar cambios</Btn>
-               </Form>
-             </div>
-             
-           </div>
-         </div>)
-       : (<Navigate to='/' replace />)
-    }
-    </div>
-
+    <>
+      {user
+        ? (<div className='flex flex-col justify-evenly h-full gap-12'>
+          <div className='flex w-full place-content-center place-items-center '>
+            <ProfileImg src={user.avatarURL} />
+            <div className='text-center font-bold text-2xl'>
+              <h1>{firstName}</h1>
+              <h1>{lastName}</h1>
+            </div>
+          </div>
+          <div className='flex flex-col w-48 mx-auto gap-4 text-xl'>
+            <Btn onClick={Navigate('/editProfile')}>Editar perfil</Btn>
+            <Btn onClick={Navigate('/appointments')}>Mis citas</Btn>
+            <Btn onClick={Navigate('/pastappointments')}>Historial de citas</Btn>
+            <Btn onClick={logout}>Log Out</Btn>
+            <Btn $dark>Eliminar usuario</Btn>
+          </div>
+        </div>)
+        : <p>Loading...</p>}
+    </>
   )
 }
-
-export default Profile
