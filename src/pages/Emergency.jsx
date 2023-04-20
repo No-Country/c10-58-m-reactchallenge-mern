0848214df74d2c834892523068c514510436ca23
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2023-04-20 19:17:09 -0300

    typo

--- src/pages/Emergency.jsx
+++ src/pages/Emergency.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Btn } from '../components/MicroComponents/Btn'
-import { MainDivText, MainDivTitle } from '../components/MicroComponents/Text'
+import { MainDivText } from '../components/MicroComponents/Text'
 
 const Emergency = () => {
   return (
     <div className='flex flex-col items-center justify-center gap-10'>
       <div className='flex flex-col place-items-center place-self-center gap-2'>
-        <MainDivText>Encaso de tener un cuadro de crisis haz click en el boton de emergencia</MainDivText>
+        <MainDivText>En caso de tener un cuadro de crisis haz click en el boton de emergencia</MainDivText>
         <img src='/public/telefono.png' alt='llamada' />
         <MainDivText>Usa esta linea de contacto adecuadamente y solo para emergencias</MainDivText>
       </div>
