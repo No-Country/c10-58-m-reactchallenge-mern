b6511cee10636b0006387f1c9021321c24fc11d1
uspaz <Matias-Teijeiro2015@hotmail.com>
2023-04-21 21:01:51 -0300

    testing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,24 @@ import BackArrow from './assets/svgs/arrowleft.svg'
 import { MainContainer } from './components/MicroComponents/Containers'
 import Navbar from './components/Navbar'
 
-function App () {
+function App() {
   const navigate = useNavigate()
+  // probando a ver si funciona
 
-  function navigateBack () {
+  function navigateBack() {
     navigate(-1)
   }
   const location = useLocation()
   const locationsNames = location.pathname.split('/')
   const isMain = locationsNames.includes('list')
   const showBackArrow =
-    location.pathname === '/' || location.pathname === '/profile' || location.pathname === '/login'
+    location.pathname === '/' ||
+    location.pathname === '/profile' ||
+    location.pathname === '/login'
   return (
-    <MainContainer className='overflow-x-hidden' isMain={!isMain}>
+    <MainContainer className="overflow-x-hidden" isMain={!isMain}>
       {!showBackArrow && (
-        <button className='absolute top-4 left-4' onClick={navigateBack}>
+        <button className="absolute top-4 left-4" onClick={navigateBack}>
           <img src={BackArrow} height={32} width={32} />
         </button>
       )}
